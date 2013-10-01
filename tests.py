8b9c9242129d17bf1a887f3008af92dcd8ce77cc
Daniele Procida <daniele@vurt.org>
2013-10-02 00:33:34 +0100

    use fingerprints to determine winningness

--- tests.py
+++ tests.py
@@ -2,8 +2,20 @@ import unittest
 
 from datetime import datetime
 
-from tictactoe import Game, Board
+from tictactoe import Game, Board, WinningLine
 
+
+class TestGame(unittest.TestCase):
+    def test_check_game_when_not_enough_moves(self):
+        game = Game(dimensions=(6,6))
+        self.assertEqual(game.check_game_state(), None)
+
+    def test_check_game_we_have_run_out_of_moves(self):
+        game = Game(dimensions=(6,6))
+        game.move = 36
+        with self.assertRaises(SystemExit) as cm:
+            game.check_game_state()
+        self.assertEqual(cm.exception.code, "It's a draw")
 
 class TestBoard(unittest.TestCase):
 
@@ -15,6 +27,36 @@ class TestBoard(unittest.TestCase):
         game = Game(dimensions=(10,10))
         self.assertEqual(len(game.board.cells), 100)
 
+    def test_create_cells(self):
+        board = Board(dimensions=(3,3))
+        for y in range(0,3):
+            for x in range(0,3):
+                self.assertTrue((x,y) in board.cells)
+                self.assertEqual(
+                    board.cells[(x,y)].board,
+                    board
+                    )
+
+    def test_get_positions_none(self):
+        board = Board(dimensions=(3,3), winning_length=3)
+        self.assertEqual(board.get_positions(player=0), [])
+
+    def test_get_positions_multiple(self):
+        board = Board(dimensions=(3,3), winning_length=3)
+        for x in range(0,3):
+            board.cells[(x,1)].mark(player=0)
+        for y in range(0,3):
+            board.cells[(1,y)].mark(player=1)
+
+        self.assertItemsEqual(
+            board.get_positions(player=0),
+            [(0, 1), (2, 1)]
+            )
+        self.assertItemsEqual(
+            board.get_positions(player=1),
+            [(1, 0), (1, 1), (1,2)]
+            )
+
     def test_create_lines(self):
         board = Board(dimensions=(0,0))
         line = board.create_line(start=(0,7), vector=(1,-1), length=8)
@@ -23,10 +65,10 @@ class TestBoard(unittest.TestCase):
             [(0, 7), (1, 6), (2, 5), (3, 4), (4, 3), (5, 2), (6, 1), (7, 0)]
         )
 
-    def test_create_winning_lines_3x3(self):
+    def test_calculate_winning_lines_3(self):
         board = Board(dimensions=(3,3), winning_length=3)
         self.assertEqual(
-            board.winning_lines,
+            board.calculate_winning_lines(),
             [
                 # 3 columns
                 [(0,0,), (0,1), (0,2)],
@@ -41,144 +83,96 @@ class TestBoard(unittest.TestCase):
                 # other diagonal
                 [(0,2,), (1,1), (2,0)],
             ]
-            )
+        )
 
-    def test_create_winning_lines_2x2(self):
-        board = Board(dimensions=(2,2), winning_length=2)
+    def test_calculate_winning_lines_2(self):
+        board = Board(dimensions=(3,3), winning_length=2)
         self.assertEqual(
-            board.winning_lines,
+            board.calculate_winning_lines(),
             [
-                # 2 columns
-                [(0,0,), (0,1)],
-                [(1,0,), (1,1)],
-                # diagonal
-                [(0,0,), (1,1)],
-                # 2 rows
-                [(0,0,), (1,0)],
-                [(0,1,), (1,1)],
-                # other diagonal
-                [(0,1,), (1,0)],
+                # vertical pairs (top)
+                [(0, 0), (0, 1)],
+                [(1, 0), (1, 1)],
+                [(2, 0), (2, 1)],
+                # vertical pairs (bottom)
+                [(0, 1), (0, 2)],
+                [(1, 1), (1, 2)],
+                [(2, 1), (2, 2)],
+                # diagonal pairs (top)
+                [(0, 0), (1, 1)],
+                [(1, 0), (2, 1)],
+                # diagonal pairs (bottom)
+                [(0, 1), (1, 2)],
+                [(1, 1), (2, 2)],
+                # horizontal pairs (top)
+                [(0, 0), (1, 0)],
+                [(1, 0), (2, 0)],
+                # horizontal pairs (middle)
+                [(0, 1), (1, 1)],
+                [(1, 1), (2, 1)],
+                # horizontal pairs (bottom)
+                [(0, 2), (1, 2)],
+                [(1, 2), (2, 2)],
+                # diagonal pairs (middle)
+                [(0, 1), (1, 0)],
+                [(1, 1), (2, 0)],
+                # diagonal pairs (bottom)
+                [(0, 2), (1, 1)],
+                [(1, 2), (2, 1)],
             ]
+        )
+
+    def test_create_winning_lines(self):
+        board = Board(dimensions=(10,10), winning_length=2)
+        self.assertEqual(
+            [wl.cells for wl in board.winning_lines],
+            board.calculate_winning_lines()
             )
 
-    # def test_winning_lines_are_correct(self):
-    #     # For a given board, check that it knows what its
-    #     # winning lines are. We can't check them all, but we can
-    #     # check a few
-    #     board = Board(dimensions=(3,3))
-    #     self.assertTrue((0,1), (0,2), (0,3) in board.winninglines)
+    def test_get_fingerprint(self):
+        board = Board(dimensions=(3,3), winning_length=3)
+        for x in range(0,3):
+            board.cells[(x,1)].mark(player=0)
+        for y in range(0,3):
+            board.cells[(1,y)].mark(player=1)
+        # player 0 has bits 3,5 set = 40
+        self.assertEqual(board.get_fingerprint(player=0), 40)
+        # player 1 has bits 1,4,7 set = 146
+        self.assertEqual(board.get_fingerprint(player=1), 146)
 
-#     def test_check_game_state_premature(self):
-#         self.assertEqual(self.game.check_game_state(), None)
-#
-#     def test_check_game_state_no_line(self):
-#         self.game.move = 6
-#         self.game.player = 1
-#         board = self.game.board
-#         board.cells[0,0].player = 1
-#         self.assertEqual(self.game.check_game_state(), None)
-#
-#     def test_check_for_winning_row(self):
-#         self.game.move = 6
-#         self.game.player = 1
-#         board = self.game.board
-#         board.cells[0,0].player = 1
-#         board.cells[2,0].player = 1
-#         board.cells[1,0].player = 1
-#         with self.assertRaises(SystemExit) as cm:
-#             self.game.check_game_state()
-#         self.assertEqual(cm.exception.code, "o wins")
-#
-#     def test_check_for_winning_column(self):
-#         self.game.move = 6
-#         self.game.player = 0
-#         board = self.game.board
-#         board.cells[1,0].player = 0
-#         board.cells[1,1].player = 0
-#         board.cells[1,2].player = 0
-#         with self.assertRaises(SystemExit) as cm:
-#             self.game.check_game_state()
-#         self.assertEqual(cm.exception.code, "x wins")
-#
-#     def test_check_for_winning_diagonal_inc(self):
-#         self.game.move = 6
-#         self.game.player = 1
-#         board = self.game.board
-#         board.cells[0,0].player = 1
-#         board.cells[1,1].player = 1
-#         board.cells[2,2].player = 1
-#         with self.assertRaises(SystemExit) as cm:
-#             self.game.check_game_state()
-#         self.assertEqual(cm.exception.code, "o wins")
-#
-#     def test_check_for_winning_diagonal_dec(self):
-#         self.game.move = 6
-#         self.game.player = 0
-#         board = self.game.board
-#         board.cells[0,2].player = 0
-#         board.cells[1,1].player = 0
-#         board.cells[2,0].player = 0
-#         with self.assertRaises(SystemExit) as cm:
-#             self.game.check_game_state()
-#         self.assertEqual(cm.exception.code, "x wins")
-#
-#     def test_check_for_draw(self):
-#         self.game.move = 9
-#         self.game.player = 0
-#         with self.assertRaises(SystemExit) as cm:
-#             self.game.check_game_state()
-#         self.assertEqual(cm.exception.code, "It's a draw")
-#
-#
-# class TestBoard(unittest.TestCase):
-#
-#     def setUp(self):
-#         self.board = Board()
-#
-#     def test_get_positions(self):
-#         board = self.board
-#
-#         # no cells marked
-#         self.assertEqual(board.get_positions(0), [])
-#
-#         # a few cells marked
-#         board.cells[2,0].player = 1
-#         board.cells[0,0].player = 1
-#         board.cells[0,1].player = 1
-#         self.assertEqual(
-#             set(board.get_positions(1)),
-#             set(((0,0), (0,1), (2,0),))
-#             )
-#
-#
-#     def test_board_has_correct_winning_line_fingerprints(self):
-#         combinations = []
-#         fingerprints = []
-#
-#         combinations.extend([[(x,y) for x in range(3)] for y in range(3)])
-#         combinations.extend([[(x,y) for y in range(3)] for x in range(3)])
-#         combinations.extend([[(xy,xy) for xy in range(3)]])
-#         combinations.extend([[(xy,2-xy) for xy in range(3)]])
-#
-#         for c in combinations:
-#             fingerprint = sum([pow(2,x+y*3) for x,y in c])
-#             fingerprints.append(fingerprint)
-#
-#         self.assertEqual(
-#             [w.fingerprint for w in self.board.winninglines],
-#             fingerprints
-#             )
-#
-#
-# class TestCell(unittest.TestCase):
-#
-#     def setUp(self):
-#         self.game = Game()
-#
-#     def test_check_for_occupied_cell(self):
-#         self.assertEqual(self.game.check_game_state(), None)
-#
-#
+    def test_check_for_winning_line(self):
+        board = Board(dimensions=(3,3), winning_length=3)
+        for x in range(0,3):
+            board.cells[(x,1)].mark(player=0)
+        self.assertTrue(board.check_for_winning_line(player=0))
+
+        for y in range(0,3):
+            board.cells[(1,y)].mark(player=1)
+        self.assertTrue(board.check_for_winning_line(player=1))
+
+
+
+class TestWinningLine(unittest.TestCase):
+    def test_has_correct_fingerprint(self):
+        """
+        We can't test every possible fingerprint, but we can provide
+        a few sample test cases
+        """
+        board = Board(dimensions=(1,3), winning_length=3)
+        # bits 0,1,2
+        w = WinningLine([(0,0,), (0,1), (0,2)], board)
+        self.assertEqual(w.fingerprint, 7)
+
+        board = Board(dimensions=(3,1), winning_length=3)
+        # bits 0,1,2
+        w = WinningLine([(0,0,), (1,0), (2,0)], board)
+        self.assertEqual(w.fingerprint, 7)
+
+        board = Board(dimensions=(3,3), winning_length=3)
+        # bits 0,5,8
+        w = WinningLine([(0,0,), (0,1), (0,2)], board)
+        self.assertEqual(w.fingerprint, 73)
+
 
 if __name__ == '__main__':
     unittest.main()
--- tictactoe.py
+++ tictactoe.py
@@ -7,8 +7,8 @@ class Game(object):
     the players, their moves and the rules through which they stand in relation
     to each other
     """
-    def __init__(self, dimensions=None, players=None):
-        self.board = Board(self, dimensions)
+    def __init__(self, dimensions=None, winning_length=0, players=None):
+        self.board = Board(self, dimensions, winning_length)
 
         self.player = 0
         self.move = 0
@@ -16,101 +16,38 @@ class Game(object):
         self.players = players
         # self.board.draw()
 
-#     def check_game_state(self):
-#         # returns None, player 0 or player 1
-#
-#         # don't even bother checking if we haven't yet had 6 moves
-#         if self.move > 5:
-#
-#             # find all the positions this player occupies
-#             positions = self.board.get_positions(self.player)
-#
-#             # find starts of all potential winning lines
-#             # they all start at x=0 or y=0
-#             for (x,y) in [p for p in positions]:
-#                 # for each starting position, find the line-mates that would
-#                 # make a winning line
-#                 all_line_mates = [
-#                     [(x+r,y) for r in range(3)],     # row
-#                     ((x,y+1), (x,y+2)),     # column
-#                     ((x+1,y+1), (x+2,y+2)), # diagonal
-#                     ((x+1,y-1), (x+2,y-2))  # other diagonal
-#                     ]
-#                 # if the player occupies all the cells in any of the line-mates,
-#                 # we have a winner
-#                 for line_mates in all_line_mates:
-#                     if set(line_mates) <= set(positions):
-#                         sys.exit("%s wins" %PLAYERS[self.player])
-#
-#         # we have exhausted all possible moves; it's a draw
-#         if self.move >= 8:
-#             sys.exit("It's a draw")
-#
-#     STRATEGIES = {
-#         "random": "play_random_move",
-#         "first": "play_first_available_move",
-#             "winning": "choose_winning_move",
-#     }
-#
-#     def play(self, strategy="random"):
-#         # so far, we only cater for 0-player games
-#         if not self.players:
-#             getattr(self, self.STRATEGIES[strategy])()
-#             self.play(strategy)
-#
-#     # different strategies for playing
-#
-#     def play_first_available_move(self):
-#         position = self.board.get_positions(None)[0]
-#         self.board.cells[position].mark()
-#
-#     def play_random_move(self):
-#         position = choice(self.board.get_positions(None))
-#         self.board.cells[position].mark()
-#
-#     def choose_winning_move(self):
-#         self.play_random_move()
-#
-#
-# class WinningLine(object):
-#     def __init__(self, cells):
-#         self.cells = cells
-#         self.fingerprint = sum([pow(2,x+y*3) for x,y in cells])
-#
+    def check_game_state(self):
+        # returns None, player 0 or player 1
+
+        # don't even bother checking if we haven't yet had enough moves
+        if self.move < self.board.winning_length * 2 - 1:
+            return
+
+        # we have exhausted all possible moves; it's a draw
+        if self.move >= self.board.x * self.board.y -1:
+            sys.exit("It's a draw")
+
+
+class WinningLine(object):
+    def __init__(self, cells, board):
+        self.cells = cells
+        self.fingerprint = sum(
+            [pow(2,x+y*board.dimensions[0])
+            for x,y in cells]
+            )
+
+
 class Board(object):
-    def __init__(self, game=None, dimensions=(), winning_length=3):
+    def __init__(self, game=None, dimensions=(), winning_length=0):
         self.game = game
+        self.winning_length=winning_length
+
         # board dimensions must be declared
         self.dimensions = dimensions or self.game.dimensions
         self.x, self.y = self.dimensions[0], self.dimensions[1]
 
         self.create_cells()
-        self.create_winning_lines(length=winning_length)
-
-    def create_winning_lines(self, length):
-        # the vectors are four of eight points of the compass
-        # we don't need their reflections
-        self.winning_lines=[]
-        vectors = ((0,1), (1,1), (1,0), (1,-1))
-
-        for v in vectors:
-            for y in range(self.y):
-                # check if line is within board dimensions
-                for x in range(self.x):
-                    if x + v[0] * length <= self.x and \
-                        y + v[1] * length <= self.y and \
-                        1 + y + v[1] * length >= 0:
-                        start = (x,y)
-                        line = self.create_line(start, v, length)
-                        self.winning_lines.append(line)
-
-    def create_line(self, start, vector, length):
-        return [
-        (
-            start[0] + vector[0] * l,
-            start[1] + vector[1] * l)
-        for l in range(length)
-        ]
+        self.create_winning_lines()
 
     def create_cells(self):
         self.cells = {}
@@ -118,56 +55,75 @@ class Board(object):
             for x in range(0,self.x):
                 self.cells[(x,y)] = Cell(self)
 
+    def create_winning_lines(self):
+        self.winning_lines = [
+            WinningLine(line, self)
+            for line in self.calculate_winning_lines()
+            ]
 
-#
-#     def draw(self):
-#         print
-#         print "\n-----\n".join(["|".join([PLAYERS[self.cells[(x,y)].player] or str(y*3+x) for x in range(0,3)]) for y in range(0,3)])
-#
-#
-#     def get_positions(self, player):
-#         # Get a list of the cells the player has moved into;
-#         # player = None for unoccupied cells
-#         return [
-#             position for position, cell in self.cells.items() if cell.player==player
-#             ]
-#
-#
-#     # untested
-#     def get_cell(self, position):
-#         return self.cells[position]
-#
-#
-#     def __str__(self):
-#         for x in range(0,3):
-#             for y in range(0,3):
-#                 print self.cells[(x,y)],
-#             print
-#         return str(self.game.player)
-#
-#
-# PLAYERS = {0: "x", 1: "o", None: ""}
-#
+    def calculate_winning_lines(self):
+        # the vectors are four of eight points of the compass
+        # we don't need their reflections
+        winning_lines=[]
+        vectors = ((0,1), (1,1), (1,0), (1,-1))
+        for v in vectors:
+            for y in range(self.y):
+                for x in range(self.x):
+                    # Check if line will be within board dimensions
+                    # These checks are coupled to the vectors above - using the
+                    # vectors' reflections would require additional checks.
+                    if x + v[0] * self.winning_length <= self.x and \
+                        y + v[1] * self.winning_length <= self.y and \
+                        1 + y + v[1] * self.winning_length >= 0:
+                        start = (x,y)
+                        winning_lines.append(
+                            self.create_line(start, v, self.winning_length)
+                            )
+        return winning_lines
+
+    def create_line(self, start, vector, length):
+        return [
+        (
+            start[0] + vector[0] * l,
+            start[1] + vector[1] * l
+            )
+        for l in range(length)
+        ]
+
+    def get_positions(self, player):
+        # Get a list of the cells the player has moved into;
+        # player = None for unoccupied cells
+        return [
+            pos for pos, cell in self.cells.items() if cell.player==player
+            ]
+
+    def get_fingerprint(self, player):
+        return sum(
+            [pow(2,x+y*self.dimensions[0])
+            for x,y in self.get_positions(player)
+            ])
+
+    def check_for_winning_line(self, player=None, cell=None):
+        if not player == None:
+            fingerprint = self.get_fingerprint(player)
+            for wl in self.winning_lines:
+                if fingerprint & wl.fingerprint:
+                    return True
+
+
 class Cell(object):
 
     def __init__(self, board):
         self.board = board
         self.player = None
-#
-#     def __repr__(self):
-#         return PLAYERS.get(self.player)
-#
-#     def mark(self):
-#         # marks a cell for a player
-#         self.player = self.board.game.player
-#         self.board.draw()
-#         if self.board.game.check_game_state() != None:
-#
-#              "Winner", self.board.game.check_game_state()
-#             sys.exit()
-#         self.board.game.player = -cmp(self.board.game.player, 1)
-#         self.board.game.move += 1
-#
-#
+
+    def mark(self, player=None):
+        # marks a cell for a player
+        if player == None:
+            self.player = self.board.game.player
+        else:
+            self.player = player
+
+
 # game = Game()
 # game.play("winning")
